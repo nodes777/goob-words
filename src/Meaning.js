320c11ee5021bd3158db78ee1db14febf0b9455b
Taylor <taylor.nodell@gmail.com>
2020-01-22 12:14:07 +1100

    adds meaning word and description in index

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,6 +3,7 @@ function Meaning(props) {
 	return (
 		<div className="goob-meaning">
 			<p>
+				Meaning:{" "}
 				{props.meaning.charAt(0).toUpperCase() + props.meaning.slice(1)}
 			</p>
 		</div>
